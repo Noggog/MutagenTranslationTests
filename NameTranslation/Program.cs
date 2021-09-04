57945e45304f99f5af97015964ca437f08972189
Justin Swanson <justin.c.swanson@gmail.com>
2021-09-04 14:01:22 -0500

    Initial code

---
+++ NameTranslation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Environments;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Strings;
+
+namespace NameTranslation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var env = GameEnvironment.Typical.Skyrim(SkyrimRelease.SkyrimSE);
+
+            var skyrimModKey = ModKey.FromFileName("Skyrim.esm");
+            var skyrim = env.LoadOrder.GetIfEnabledAndExists(skyrimModKey);
+            var massParalysis = skyrim.Books.First(x => x.EditorID == "SpellTomeMassParalysis");
+            ReportBook(skyrimModKey, massParalysis);
+        }
+
+        private static void ReportBook(ModKey modKey, IBookGetter book)
+        {
+            if (book.Name == null) return;
+            var name = book.Name;
+            
+            Console.WriteLine($"Name for {book} from {modKey} is {name}");
+            Console.WriteLine($"Had {book.Name.NumLanguages} registered languages");
+                
+            if (book.Name.TryLookup(Language.French, out var fre))
+            {
+                Console.WriteLine($"Had name in French: {fre}");
+            }
+
+            var outgoing = new SkyrimMod("Outgoing.esp", SkyrimRelease.SkyrimSE);
+            var bookW = outgoing.Books.GetOrAddAsOverride(book);
+            bookW.Name = $"{fre}Test";
+            outgoing.WriteToBinary(outgoing.ModKey.FileName.ToString());
+        }
+    }
+}
